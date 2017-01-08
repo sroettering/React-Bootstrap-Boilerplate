6f39d94d45d56100997e12ba72ff9ba231e8b800
Sven Röttering <svenroettering@googlemail.com>
2017-01-08 17:24:17 +0100

    modified not-found page

--- imports/modules/view/not-found.jsx
+++ imports/modules/view/not-found.jsx
@@ -1,3 +1,7 @@
 import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
 
-export default NotFound = () => <div className="not-found">404 Not found</div>
+export default NotFound = () => <Jumbotron>
+  <h1>Page not found</h1>
+  <p>Are you sure you entered the correct URL?</p>
+</Jumbotron>
